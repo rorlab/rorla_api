7695e38cdbe951f354e56afb7665b0f3db1c7a9d
Hyo Seong Choi <rorlab@gmail.com>
2014-03-08 14:01:12 +0900

    deploy 옵션추가

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,16 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'rorla_api'
+set :user, 'deployer'
+set :repo_url, 'git@github.com:hschoidr/rorla_api.git'
 
 # Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
 # set :deploy_to, '/var/www/my_app'
+set :deploy_to, "/home/#{user}/apps/#{application}"
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -47,7 +49,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
